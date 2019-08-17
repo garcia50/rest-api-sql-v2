b3e7f88d17c644f9841362f587a4f4fb12c0eaa4
Luis Garcia <garcia50@yahoo.com>
2019-08-17 16:13:34 -0400

    Update authentication for user middleware

--- routes/users.js
+++ routes/users.js
@@ -7,43 +7,33 @@ const { check, validationResult } = require('express-validator');
 
 const authenticateUser = (req, res, next) => {
   let message = null;
+  // Parse the user's credentials from the Authorization header.
   const credentials = auth(req);
-
   if (credentials) {
     User.findOne({ where: {emailAddress: credentials.name} })
-      .then(function(u) {
-        const user = u.dataValues;
-
-        if (user) {
-          const authenticated = bcryptjs
-          .compareSync(credentials.pass, user.password);
-          if (authenticated) {
-            console.log(`Authentication successful for username: ${user.firstName} ${user.lastName}`);
-            req.currentUser = user;
-          } else {
-            message = `Authentication failure for username: ${user.emailAddress}`;
-          }
+    .then((user) => {
+      if (user) {
+        const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+        if (authenticated) {
+          console.log(`Authentication successful for username: ${user.firstName} ${user.lastName}`);
+          req.currentUser = user;
+          next();
         } else {
-          message = `User not found for username: ${credentials.name}`;
+          message = `Authentication failure for username: ${user.emailAddress}`;
+          res.status(401).json({ message: message });
         }
-      })
+      } else {
+        message = 'Please enter user credentials';
+        res.status(401).json({ message: message });
+      }
+    });
   } else {
     message = 'Auth header not found';
+    res.status(401).json({ message: message });
   }
-
-  // If user authentication failed...
-  // if (message) {
-  //   console.warn(message);
-
-  //   // Return a response with a 401 Unauthorized HTTP status code.
-  //   res.status(401).json({ message: 'Access Denied' });
-  // } else {
-  //   // Or if user authentication succeeded...
-  //   // Call the next() method.
-  //   next();
-  // }
-
 }
+
 
 function asyncHandler(cb) {
   return async (req, res, next) => {
@@ -56,15 +46,13 @@ function asyncHandler(cb) {
 }
 
 
-router.get('/', authenticateUser, asyncHandler( async (req, res) => {
-  console.log('cuuurent uuusserererere', req.currentUser);
+router.get('/', authenticateUser, (req, res) => {
   const user = req.currentUser;
-
-  res.json({
-    name: user.name,
-    username: user.username,
+  res.status(200).json({
+    name: user.firstName,
+    username: user.emailAddress
   });
-}));
+});
 
 
 router.post('/', [
@@ -80,60 +68,37 @@ router.post('/', [
   check('password')
     .exists()
     .withMessage('Please provide a value for "Password"')
-], asyncHandler( async (req, res) => {
+  ], 
 
-  // Attempt to get the validation result from the Request object.
-  const errors = validationResult(req);
+  asyncHandler( async (req, res) => {
+    // Attempt to get the validation result from the Request object.
+    const errors = validationResult(req);
 
-  // If there are validation errors...
-  if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
-    const errorMessages = errors.array().map(error => error.msg);
-    // Return the validation errors to the client.
-    return res.status(400).json({ errors: errorMessages });
+    // If there are validation errors...
+    if (!errors.isEmpty()) {
+      // Use the Array `map()` method to get a list of error messages.
+      const errorMessages = errors.array().map(error => error.msg);
+      // Return the validation errors to the client.
+      return res.status(400).json({ errors: errorMessages });
 
-  } else if (req.body.firstName && req.body.lastName 
-      && req.body.emailAddress && req.body.password) {
+    } else if (req.body.firstName && req.body.lastName 
+        && req.body.emailAddress && req.body.password) {
 
-    req.body.password = bcryptjs.hashSync(req.body.password);
-    
-    const user = await User.create(req.body);
+      req.body.password = bcryptjs.hashSync(req.body.password);
+      
+      const user = await User.create(req.body);
 
-    res.location('/').status(201).json(user);
+      res.location('/').status(201).json(user);
 
-  } else {
-    res.status(400).json({message: "First name, last name, email address, password is required."});
-  }
-
-}));
+    } else {
+      res.status(400).json({message: "First name, last name, email address, password is required."});
+    }
+  })
+);
 
 
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
